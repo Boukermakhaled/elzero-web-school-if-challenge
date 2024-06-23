cc07896a63ed94d7ca4aa43de040548632897d8b
Khaled Abderrahmane <125027998+Boukermakhaled@users.noreply.github.com>
2024-06-23 22:41:51 +0200

    elzero srcond if challenge

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ a < 10 ? console.log(10)
 : a >= 10 && a <= 40 ? console.log("10 to 40") 
 : a > 40 ? console.log("bigger than 40") 
 : console.log("unknow");
+/////
+
+let st="Elzero web school";
+
+if(( st.length * 2).toString() === "34"){
+    console.log("good");
+}
+if( st.charAt(st.indexOf("w")) === "w"){
+    console.log("good");
+}
+if( st.length !== "string"){
+    console.log("good");
+}
+if( typeof(st.length) === "number"){
+    console.log("good");
+}
+if( (st.substring(0,6)).repeat(2) === "ElzeroElzero"){
+    console.log("good");
+}
